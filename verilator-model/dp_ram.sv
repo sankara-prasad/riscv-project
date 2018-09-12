e306daab34a4b8895af654418123634ad7905bf6
akhi1997 <43198980+akhi1997@users.noreply.github.com>
2018-09-12 13:01:18 +0530

    Update dp_ram.sv

--- verilator-model/dp_ram.sv
+++ verilator-model/dp_ram.sv
@@ -8,6 +8,7 @@
 // this License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
 // CONDITIONS OF ANY KIND, either express or implied. See the License for the
 // specific language governing permissions and limitations under the License.
+// shit just got real bitch
 
 module dp_ram
   #(
